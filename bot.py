8240c4b9119b046b144110ba7f1b855d69ce232f
Aeek True <aeektrue@gmail.com>
2022-07-08 15:05:13 +0300

    use main_keyboard

--- bot.py
+++ bot.py
@@ -2,19 +2,20 @@ import telebot
 from telebot import types
 
 import settings
-
+from bot_keyboards import main_keyboard
 
 bot = telebot.TeleBot(settings.TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def start(msg: types.Message):
-	bot.send_message(msg.chat.id, f"""Hello, {msg.from_user.first_name}!
-	I'm WeatherBot, you weather informer!
-	Send me /howisweather to get weather""")
+	bot.send_message(chat_id=msg.chat.id,
+	                 text=f"""Hello, {msg.from_user.first_name}!
+	I'm WeatherBot, weather informer!
+	Send me /howisweather to get weather""",
+	                 reply_markup=main_keyboard)
 
 
 @bot.message_handler(commands=['howisweather'])
 def weather(msg: types.Message):
 	bot.send_message(msg.chat.id, "This feature coming soon.")
-	
