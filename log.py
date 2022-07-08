b91edc8990da6a8f0d9b0492149955c072cdd7cc
Aeek True <aeektrue@gmail.com>
2022-07-08 14:17:45 +0300

    Add log module

---
+++ log.py
@@ -0,0 +1,4 @@
+import sys
+from loguru import logger
+
+logger.add(sys.stderr, format='[{time:YYYY-MM-DD HH:mm:ss}] {level: <8} - {message}')
--- webhook_setup.py
+++ webhook_setup.py
@@ -1,5 +1,7 @@
 from bot import bot
 from settings import WEBHOOK_URL, WEBHOOK_PATH
+from log import logger
 
 bot.remove_webhook()
 bot.set_webhook(WEBHOOK_URL + WEBHOOK_PATH)
+logger.info(f'Set webhook: {WEBHOOK_URL + WEBHOOK_PATH}')
