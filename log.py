ab895fab0bcc5e326d6205d31213b4fe4cc33fe9
Aeek True <aeektrue@gmail.com>
2022-07-08 15:05:35 +0300

    change logger format

--- log.py
+++ log.py
@@ -1,4 +1,4 @@
 import sys
 from loguru import logger
 
-logger.add(sys.stderr, format='[{time:YYYY-MM-DD HH:mm:ss}] {level: <8} - {message}')
+logger.add(sys.stderr, format='[{time:YYYY-MM-DD HH:mm:ss}] {level: <8}: {message}')
