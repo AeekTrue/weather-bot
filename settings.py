e6fd49c637a9ff261734db6403e8b230eb8af557
Aeek True <aeektrue@gmail.com>
2022-07-08 14:17:28 +0300

    Fix webhook URL

--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@ from os import environ
 TOKEN = environ["TELEBOT_TOKEN"]
 CREATOR_ID = environ.get("TELEBOT_OWNER_ID")
 
-WEBHOOK_URL = f"https://howisweatherbot.herokuapp.com/"
+WEBHOOK_URL = f"https://howisweatherbot.herokuapp.com"
 WEBHOOK_PATH = f"/{TOKEN}/".replace(':', '').replace('-', '')
